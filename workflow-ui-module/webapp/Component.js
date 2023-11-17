fe5bebfecd47b73d031baf716cf244f2be6fd2f5
Vladimir Vuckovic <vidrak97@gmail.com>
2023-11-17 10:57:30 +0000

    changed to accomodate form

--- workflow-ui-module/webapp/Component.js
+++ workflow-ui-module/webapp/Component.js
@@ -1,25 +1,25 @@
 sap.ui.define(
   [
-    "sap/ui/core/UIComponent",
-    "sap/ui/Device",
-    "ordersmanagement/workflowuimodule/model/models",
+      "sap/ui/core/UIComponent",
+      "sap/ui/Device",
+      "ordersmanagement/workflowuimodule/model/models",
   ],
   function (UIComponent, Device, models) {
-    "use strict";
+      "use strict";
 
-    return UIComponent.extend(
+      return UIComponent.extend(
       "ordersmanagement.workflowuimodule.Component",
       {
-        metadata: {
+          metadata: {
           manifest: "json",
-        },
+          },
 
-        /**
-         * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
-         * @public
-         * @override
-         */
-        init: function () {
+          /**
+          * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+          * @public
+          * @override
+          */
+          init: function () {
           // call the base component's init function
           UIComponent.prototype.init.apply(this, arguments);
 
@@ -31,113 +31,117 @@ sap.ui.define(
 
           this.setTaskModels();
 
+          const rejectOutcomeId = "reject";
           this.getInboxAPI().addAction(
-            {
-              action: "APPROVE",
-              label: "Approve",
-              type: "accept", // (Optional property) Define for positive appearance
-            },
-            function () {
-              this.completeTask(true);
-            },
-            this
-          );
-
-          this.getInboxAPI().addAction(
-            {
-              action: "REJECT",
+              {
+              action: rejectOutcomeId,
               label: "Reject",
-              type: "reject", // (Optional property) Define for negative appearance
-            },
-            function () {
-              this.completeTask(false);
-            },
-            this
+              type: "reject",
+              },
+              function () {
+              this.completeTask(false, rejectOutcomeId);
+              },
+              this
           );
-        },
+          const approveOutcomeId = "approve";
+          this.getInboxAPI().addAction(
+              {
+              action: approveOutcomeId,
+              label: "Approve",
+              type: "accept",
+              },
+              function () {
+              this.completeTask(true, approveOutcomeId);
+              },
+              this
+          );
+          },
 
-        setTaskModels: function () {
+          setTaskModels: function () {
           // set the task model
           var startupParameters = this.getComponentData().startupParameters;
           this.setModel(startupParameters.taskModel, "task");
 
           // set the task context model
           var taskContextModel = new sap.ui.model.json.JSONModel(
-            this._getTaskInstancesBaseURL() + "/context"
+              this._getTaskInstancesBaseURL() + "/context"
           );
           this.setModel(taskContextModel, "context");
-        },
+          },
 
-        _getTaskInstancesBaseURL: function () {
+          _getTaskInstancesBaseURL: function () {
           return (
-            this._getWorkflowRuntimeBaseURL() +
-            "/task-instances/" +
-            this.getTaskInstanceID()
+              this._getWorkflowRuntimeBaseURL() +
+              "/task-instances/" +
+              this.getTaskInstanceID()
           );
-        },
+          },
 
-        _getWorkflowRuntimeBaseURL: function () {
+          _getWorkflowRuntimeBaseURL: function () {
           var appId = this.getManifestEntry("/sap.app/id");
           var appPath = appId.replaceAll(".", "/");
           var appModulePath = jQuery.sap.getModulePath(appPath);
 
           return appModulePath + "/bpmworkflowruntime/v1";
-        },
+          },
 
-        getTaskInstanceID: function () {
+          getTaskInstanceID: function () {
           return this.getModel("task").getData().InstanceID;
-        },
+          },
 
-        getInboxAPI: function () {
+          getInboxAPI: function () {
           var startupParameters = this.getComponentData().startupParameters;
           return startupParameters.inboxAPI;
-        },
+          },
 
-        completeTask: function (approvalStatus) {
+          completeTask: function (approvalStatus, outcomeId) {
           this.getModel("context").setProperty("/approved", approvalStatus);
-          this._patchTaskInstance();
-          this._refreshTaskList();
-        },
+          this._patchTaskInstance(outcomeId);
+          },
 
-        _patchTaskInstance: function () {
+          _patchTaskInstance: function (outcomeId) {
+          const context = this.getModel("context").getData();
           var data = {
-            status: "COMPLETED",
-            context: this.getModel("context").getData(),
+              status: "COMPLETED",
+              context: {...context, comment: context.comment || ''},
+              decision: outcomeId
           };
 
           jQuery.ajax({
-            url: this._getTaskInstancesBaseURL(),
-            method: "PATCH",
-            contentType: "application/json",
-            async: false,
-            data: JSON.stringify(data),
-            headers: {
+              url: `${this._getTaskInstancesBaseURL()}`,
+              method: "PATCH",
+              contentType: "application/json",
+              async: true,
+              data: JSON.stringify(data),
+              headers: {
               "X-CSRF-Token": this._fetchToken(),
-            },
-          });
-        },
+              },
+          }).done(() => {
+              this._refreshTaskList();
+          })
+          },
 
-        _fetchToken: function () {
+          _fetchToken: function () {
           var fetchedToken;
 
           jQuery.ajax({
-            url: this._getWorkflowRuntimeBaseURL() + "/xsrf-token",
-            method: "GET",
-            async: false,
-            headers: {
+              url: this._getWorkflowRuntimeBaseURL() + "/xsrf-token",
+              method: "GET",
+              async: false,
+              headers: {
               "X-CSRF-Token": "Fetch",
-            },
-            success(result, xhr, data) {
+              },
+              success(result, xhr, data) {
               fetchedToken = data.getResponseHeader("X-CSRF-Token");
-            },
+              },
           });
           return fetchedToken;
-        },
+          },
 
-        _refreshTaskList: function () {
+          _refreshTaskList: function () {
           this.getInboxAPI().updateTask("NA", this.getTaskInstanceID());
-        },
+          },
       }
-    );
+      );
   }
 );
